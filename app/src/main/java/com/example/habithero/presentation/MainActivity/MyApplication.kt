0f2c79c0b763b066756ff4871aaa25429a459d54
mac <nuridinov.abdul@yandex.ru>
2026-08-17 01:14:50 +0300

    sort modules of di

--- app/src/main/java/com/example/habithero/presentation/MainActivity/MyApplication.kt
+++ app/src/main/java/com/example/habithero/presentation/MainActivity/MyApplication.kt
@@ -3,6 +3,7 @@ package com.example.habithero.presentation.MainActivity
 import android.app.Application
 import com.example.habithero.di.dataModules.dataModule
 import com.example.habithero.di.domainModules.domainModule
+import com.example.habithero.presentation.di.uiModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -11,7 +12,7 @@ class MyApplication: Application() {
         super.onCreate()
         startKoin {
             androidContext(this@MyApplication)
-            modules(dataModule, domainModule)
+            modules(dataModule, domainModule, uiModule)
         }
     }
 }
--- app/src/main/java/com/example/habithero/presentation/di/uiModule.kt
+++ app/src/main/java/com/example/habithero/presentation/di/uiModule.kt
@@ -1,16 +1,15 @@
-package com.example.habithero.di.dataModules
+package com.example.habithero.presentation.di
 
 import com.example.habithero.domain.source.homescreen.counterHabit.CounterHabitRepository
 import com.example.habithero.domain.source.homescreen.habitsActs.UsersRepository
 import com.example.habithero.domain.source.todolist.InterPackAddRepository
 import com.example.habithero.domain.source.todolist.TodoListRepository
 import com.example.habithero.domain.usecase.UseCasesForHomeScreen.counterHabit.CounterHabitUsecase
-import com.example.habithero.domain.usecase.UseCasesForTodoListScreen.CreateTodoUseCase
 import com.example.habithero.domain.usecase.UseCasesForHomeScreen.habitsActs.DeleteUsersHabitUseCase
 import com.example.habithero.domain.usecase.UseCasesForHomeScreen.habitsActs.FetchDataUserUseCase
 import com.example.habithero.domain.usecase.UseCasesForHomeScreen.habitsActs.UpdateUsersHabitUseCase
+import com.example.habithero.domain.usecase.UseCasesForTodoListScreen.CreateTodoUseCase
 import com.example.habithero.domain.usecase.UseCasesForTodoListScreen.InterPack.InterPackAddUseCase
-import com.example.habithero.infrastructure.data.Room.Database.TodoDatabase
 import com.example.habithero.presentation.ViewModel.CounterHabitViewModel
 import com.example.habithero.presentation.ViewModel.InterPackViewModel
 import com.example.habithero.presentation.ViewModel.TodoListViewModel
@@ -19,19 +18,32 @@ import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val dataModule = module {
-
-    single {
-        TodoDatabase.getDatabaseToTodo(androidApplication()).todoDao()
+val uiModule = module {
+    viewModel{
+        CounterHabitViewModel(
+            get<CounterHabitUsecase>(),
+            get<CounterHabitRepository>()
+        )
     }
-    single {
-        TodoDatabase.getDatabaseToTodo(androidApplication()).interPackDao()
+    viewModel{(folderId: Long) ->
+        InterPackViewModel(androidApplication(),
+            get<InterPackAddRepository>(),
+            get<InterPackAddUseCase>(),
+            folderId
+        )
     }
-    single {
-        TodoDatabase.getDatabaseToTodo(androidApplication()).userDao()
-    }
-
-    single {
-        TodoDatabase.getDatabaseToTodo(androidApplication()).habitCounterDao()
+    viewModel {
+        UserViewModel(androidApplication(),
+            get<FetchDataUserUseCase>(),
+            get<DeleteUsersHabitUseCase>(),
+            get<UpdateUsersHabitUseCase>(),
+            get<UsersRepository>()
+        ) }
+    viewModel{
+        TodoListViewModel(androidApplication(),
+            get<CreateTodoUseCase>(),
+            get<TodoListRepository>(),
+            it.get()
+        )
     }
 }
